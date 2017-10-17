8a9eaa4efdc93a8c4550a4539517f04dd9900e58
Julien Amoros <ju.amoros@laposte.net>
2017-10-17 12:11:25 +0200

    basic window

--- src/main.java
+++ src/main.java
@@ -1,6 +1,13 @@
+import controller.MainController;
+
 /**
  * Created by JuAmo_000 on 17/10/2017.
  */
 public class main {
+    public static void main(final String[] args) throws Exception {
 
+
+        MainController controller = new MainController();
+
+    }
 }
--- src/view/Window.java
+++ src/view/Window.java
@@ -1,7 +1,29 @@
 package view;
 
+import javax.swing.*;
+
 /**
  * Created by JuAmo_000 on 17/10/2017.
  */
 public class Window {
+    private JPanel mainPane;
+    private JPanel inputPane;
+    private JPanel serveurPanel;
+    private JPanel clientsPanel;
+    private JPanel arrivePanel;
+    private JPanel departPanel;
+    private JTextField lambda_field;
+    private JTextField mu_field;
+    private JTextField serveur_field;
+    private JRadioButton serverRadioBtn;
+    private JTextField clients_max_field;
+    private JRadioButton clientsRadioBtn;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Window");
+        frame.setContentPane(new Window().mainPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
