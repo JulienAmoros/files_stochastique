6eff4ae1ca2fe43ae5feaf91d0245178fd573fa4
antoine@weecop.fr <geeu4j2u3b>
2017-10-17 12:27:28 +0200

    functions

--- src/model/MMS.java
+++ src/model/MMS.java
@@ -5,10 +5,10 @@ public class MMS{
     private float lambda;
     private float mu;
     private float ro;
-    private float s; //Number of servers
+    private int s; //Number of servers
 
     /** Constructor **/
-    public MMS(float lambda, float mu, float s) {
+    public MMS(float lambda, float mu, int s) {
         this.lambda = lambda;
         this.mu = mu;
         this.s = s;
@@ -16,9 +16,9 @@ public class MMS{
     }
 
     /** Getter **/
-    public float getLambda() {return lambda;}
-    public float getMu() {return mu;}
-    public float getRo() {return ro;}
+    private float getLambda() {return lambda;}
+    private float getMu() {return mu;}
+    private float getRo() {return ro;}
 
 
     /** Setter **/
@@ -32,11 +32,49 @@ public class MMS{
     }
 
     /** Function **/
-    private void MAJRo(){
-        ro = (this.getLambda()/this.getMu());
+    public static int factorielleRecursive(int n) {
+        if (n>1)
+            return n*factorielleRecursive(n-1);
+        else
+            return 1;
     }
-    private float getL(){return (this.getLambda()/(this.getMu()-this.getLambda()));}
-    private float getLq(){return ((this.getLambda()*this.getLambda())/(this.getMu()-this.getLambda()));}
-    private float getW(){return (1/(this.getMu()-this.getLambda()));}
-    private float getWq(){return (this.getLambda()/(this.getMu()*(this.getMu()-this.getLambda())));}
+
+
+
+    private void MAJRo(){
+        ro = (this.getLambda()/(s*this.getMu()));
+    }
+    private double getQ0(){
+        double denominator=0;
+        for(int i=0; i<s;i++){
+            denominator += (Math.pow(ro*s,s)/i)+(Math.pow(ro*s,s)/(factorielleRecursive(s)*(1-ro)));
+        }
+        return 1/denominator;
+    }
+
+    private double getQi(int i){
+        if(i<1){
+            return (Math.pow(s*ro,i)/factorielleRecursive(i))*getQ0();
+        }else{
+            return (Math.pow(s,s)*Math.pow(ro,i))/factorielleRecursive(s);
+        }
+    }
+
+    private double getLq(){
+        return getQ0()*(Math.pow(ro*s,ro)*ro)/(factorielleRecursive(s)*(Math.pow(1-ro,2)));
+    }
+
+    private double getWq(){
+        return getLq()/lambda;
+    }
+    private double getW(){
+        return getWq()/(1/lambda);
+    }
+
+    private double getL(){
+        return lambda/getW();
+    }
+
+
+
 }
