e74241e39b59dd404d00dbaf84fb1df84e302d1c
antoine@weecop.fr <geeu4j2u3b>
2017-10-17 12:06:32 +0200

    functions

--- src/model/MM1K.java
+++ src/model/MM1K.java
@@ -8,7 +8,7 @@ public class MM1K{
     private float k;
 
     /** Constructor **/
-    public MM1(float lambda, float mu, float k) {
+    public MM1K(float lambda, float mu, float k) {
         this.lambda = lambda;
         this.mu = mu;
         this.ro = (lambda/mu);
@@ -16,10 +16,10 @@ public class MM1K{
     }
 
     /** Getter **/
-    public float getLambda() {return lambda;}
-    public float getMu() {return mu;}
-    public float getRo() {return ro;}
-    public float getK() {return k;}
+    private float getLambda() {return lambda;}
+    private float getMu() {return mu;}
+    private float getRo() {return ro;}
+    private float getK() {return k;}
 
 
     /** Setter **/
@@ -33,10 +33,27 @@ public class MM1K{
     }
 
     /** Function **/
-    private MAJRo(){this.getRo() = (this.getLambda()/this.getMu());}
-    private getQi(){
+    private void MAJRo(){
+        ro = (this.getLambda()/this.getMu());
+    }
+
+    private double getQi(float i){
         if(getRo()==1){
             return (1/(getK()+1));
+        }else{
+            return ( (1-ro)*Math.pow(ro,i)/(1-(Math.pow(ro,k+1))) );
         }
+    }
+    private double getL(){
+        if(ro==1){
+            return k/2;
+        }else{
+            double numerator = ro*(1-(k+1)*Math.pow(ro,k)+k*Math.pow(ro,k+1));
+            double denominator = (1-ro)*(1-Math.pow(ro,k+1));
+            return numerator / denominator;
+        }
+    }
+    private double getLq(){
+        return getL()-(1-getQi(0));
     }
 }
