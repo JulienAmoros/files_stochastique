a656d5797a92740a0af8148784838b5d574cec20
Julien Amoros <ju.amoros@laposte.net>
2017-10-17 11:49:34 +0200

    initial commit

---
+++ src/main.java
@@ -0,0 +1,6 @@
+/**
+ * Created by JuAmo_000 on 17/10/2017.
+ */
+public class main {
+
+}
---
+++ src/view/Window.java
@@ -0,0 +1,7 @@
+package view;
+
+/**
+ * Created by JuAmo_000 on 17/10/2017.
+ */
+public class Window {
+}
